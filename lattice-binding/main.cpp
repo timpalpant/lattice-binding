fa30901a94417d3043849ade60947cc41ee4d414
Timothy Palpant <tim@palpant.us>
2013-03-16 19:16:57 -0400

    Initial Commit

---
+++ lattice-binding/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  lattice-binding
+//
+//  Created by Timothy Palpant on 3/16/13.
+//  Copyright (c) 2013 Timothy Palpant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
